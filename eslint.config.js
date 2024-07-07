82fc40280f924e1f80461553cb3e120a72b137ad
Mel Dommer <mel.dommer@gmail.com>
2024-07-07 13:21:55 -0500

    updating to newer packages eslint 8 fixing eslint config removing some deprecated typescript ling plugins for the new consolidated one.

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+
+export default [
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    ignores: ["node_modules", "dist", "build", "out", "coverage"],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+];
