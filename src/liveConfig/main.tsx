e53cd3881361e9f369c437a3c7ed7a10e7b06522
wembley <wembleyos@duck.com>
2025-03-13 07:07:00 +0100

    🎅🏻

--- src/liveConfig/main.tsx
+++ src/liveConfig/main.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import LiveConfig from "./LiveConfig";
-import "@/index.css";
+import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- src/mobile/main.tsx
+++ src/mobile/main.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { Mobile } from "./Mobile";
-import "@/index.css";
+import "@/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
